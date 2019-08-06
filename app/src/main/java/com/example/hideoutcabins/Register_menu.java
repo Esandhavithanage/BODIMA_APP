6f2fa10d6ecc671203358015dfcb93dd4e3303f3
EsDiViHub <esandha.d.vithanage@gmail.com>
2019-08-06 18:06:50 +0530

    Login page and registermenu

---
+++ app/src/main/java/com/example/hideoutcabins/Register_menu.java
@@ -0,0 +1,14 @@
+package com.example.hideoutcabins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_menu);
+    }
+}
