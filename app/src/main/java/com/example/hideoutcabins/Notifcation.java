1f5ef754ec22c220f5ea22d026403e2aff5e216e
sasindu  senanayake <senanayakesasindu@gmail.com>
2019-08-07 04:32:36 +0530

    Adding notification UI windows.Rating window.

---
+++ app/src/main/java/com/example/hideoutcabins/Notifcation.java
@@ -0,0 +1,16 @@
+package com.example.hideoutcabins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notifcation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifcation);
+    }
+
+
+    }
